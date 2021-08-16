b0777d4835841037e0499bcf2486daf6c801f948
InnaTovstik <kitimail69@gmail.com>
2021-08-16 20:14:30 +0300

    Add code

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        tree.add(1);
+        tree.add(95);
+        tree.add(32);
+        tree.add(11);
+        tree.add(6);
+        tree.add(8);
+        tree.add(17);
+        tree.add(45);
+        tree.add(33);
+        tree.print();
+    }
+}
+
